638ae3e51833ccdf5b5771b7175a283aef2b2c53
Yoji Shidara <dara@shidara.net>
2017-02-15 11:43:31 +0900

    Discard changes when leave edit

--- frontend/app/routes/sparqlets/edit.js
+++ frontend/app/routes/sparqlets/edit.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model({sparqlet_id}) {
     return this.get('store').findRecord('sparqlet', sparqlet_id);
+  },
+  actions: {
+    willTransition(transition) {
+      this._super(...arguments);
+      this.get('controller.model').rollbackAttributes();
+    }
   }
 });
