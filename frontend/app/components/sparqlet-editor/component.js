fdccd925086a044c62a046fcad2f2d2896c9933c
Yoji Shidara <dara@shidara.net>
2017-02-16 10:49:13 +0900

    Emphasize button when updated

--- frontend/app/components/sparqlet-editor/component.js
+++ frontend/app/components/sparqlet-editor/component.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   actions: {
     update(newCode) {
+      this.set('isUpdated', newCode !== this.attrs.src.value);
       this.set('code', newCode);
     },
     save() {
