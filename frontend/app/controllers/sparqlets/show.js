450b7a27a4e580fcd16a02d0067a68292ddfd075
Yoji Shidara <dara@shidara.net>
2017-02-20 10:55:48 +0900

    Implement SPARQLet removal

--- frontend/app/controllers/sparqlets/show.js
+++ frontend/app/controllers/sparqlets/show.js
@@ -1,5 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  session: Ember.inject.service('session')
+  session: Ember.inject.service('session'),
+
+  actions: {
+    delete(model) {
+      if (!confirm('Are you sure?')) {
+        return;
+      }
+      model.destroyRecord().then(() => {
+        this.transitionToRoute('sparqlets');
+      }).catch((err) => {
+        // TODO handle
+        console.error(err);
+      });
+    }
+  }
 });
