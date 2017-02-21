6463dfa193cf54174752ecae363508428724c529
Yoji Shidara <dara@shidara.net>
2017-02-21 12:54:01 +0900

    Use default behavior

--- frontend/app/routes/sparqlets/edit.js
+++ frontend/app/routes/sparqlets/edit.js
@@ -2,9 +2,6 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model({sparqlet_id}) {
-    return this.get('store').findRecord('sparqlet', sparqlet_id);
-  },
   actions: {
     willTransition(transition) {
       this._super(...arguments);
