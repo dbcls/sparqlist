b9eb2955b2dc11c0a44fd6f09b7a0873e19f9bae
Keita Urashima <ursm@ursm.jp>
2019-02-07 17:12:27 +0900

    Fix deprecated authorizer used

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,8 +1,18 @@
 import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'sparqlist/config/environment';
+import { inject as service } from '@ember/service';
 
 export default JSONAPIAdapter.extend(DataAdapterMixin, {
+  session: service(),
+
   namespace: `${ENV.rootURL}-api`,
-  authorizer: 'ember-simple-auth@authorizer:oauth2-bearer'
+
+  authorize(xhr) {
+    const token = this.get('session.data.authenticated.access_token');
+
+    if (token) {
+      xhr.setRequestHeader('Authorization', `Bearer ${token}`);
+    }
+  }
 });
