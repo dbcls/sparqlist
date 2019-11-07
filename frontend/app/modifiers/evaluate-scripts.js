d089172ab4c254caafe3ce34124678b362e7176a
Keita Urashima <ursm@ursm.jp>
2019-11-08 07:25:25 +0900

    Replace ScriptableHtmlEmbedding component with evaluate-scripts modifier

---
+++ frontend/app/modifiers/evaluate-scripts.js
@@ -0,0 +1,15 @@
+import { modifier } from 'ember-modifier';
+
+export default modifier(function evaluateScripts(element) {
+  element.querySelectorAll('script').forEach((orig) => {
+    const el = document.createElement('script');
+
+    el.textContent = orig.textContent;
+
+    Array.from(orig.attributes).forEach((attr) => {
+      el.setAttribute(attr.nodeName, attr.textContent);
+    });
+
+    orig.replaceWith(el);
+  });
+});
