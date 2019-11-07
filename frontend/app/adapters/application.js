1a364d70b91acac05f1165b674a64ffb20b64ea7
Keita Urashima <ursm@ursm.jp>
2019-11-08 02:51:09 +0900

    npx ember-data-codemod app

--- frontend/app/adapters/application.js
+++ frontend/app/adapters/application.js
@@ -1,6 +1,6 @@
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import ENV from 'sparqlist/config/environment';
-import JSONAPIAdapter from 'ember-data/adapters/json-api';
+import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
--- frontend/app/models/sparqlet.js
+++ frontend/app/models/sparqlet.js
@@ -1,13 +1,13 @@
-import DS from 'ember-data';
+import Model, { attr } from '@ember-data/model';
 
-export default DS.Model.extend({
-  name:             DS.attr('string'),
-  title:            DS.attr('string'),
-  html:             DS.attr('string'),
-  apiPath:          DS.attr('string'),
-  traceModeApiPath: DS.attr('string'),
-  params:           DS.attr(),
-  src:              DS.attr('string'),
-  mdPath:           DS.attr('string'),
-  mtime:            DS.attr('string'),
+export default Model.extend({
+  name:             attr('string'),
+  title:            attr('string'),
+  html:             attr('string'),
+  apiPath:          attr('string'),
+  traceModeApiPath: attr('string'),
+  params:           attr(),
+  src:              attr('string'),
+  mdPath:           attr('string'),
+  mtime:            attr('string'),
 });
