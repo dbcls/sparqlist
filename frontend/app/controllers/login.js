09389cdad746c3b218c306309233eb144203495a
Keita Urashima <ursm@ursm.jp>
2019-11-08 03:33:14 +0900

    Fix display of authentication error message

--- frontend/app/controllers/login.js
+++ frontend/app/controllers/login.js
@@ -5,11 +5,13 @@ export default Controller.extend({
   session: service('session'),
 
   actions: {
-    authenticate() {
-      // NOTE identification is not used
-      this.session.authenticate('authenticator:oauth2', '', this.password).catch((reason) => {
-        this.set('errorMessage', reason.error || reason);
-      });
+    async authenticate() {
+      try {
+        // NOTE identification is not used
+        await this.session.authenticate('authenticator:oauth2', '', this.password);
+      } catch (res) {
+        this.set('errorMessage', res.responseJSON.error || res);
+      }
     }
   }
 });
