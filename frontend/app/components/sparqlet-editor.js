8fe0e0a9b06565feeb3290ed103f5499e5a5bd4d
Keita Urashima <ursm@ursm.jp>
2019-11-09 00:37:11 +0900

    Refactor using mut

--- frontend/app/components/sparqlet-editor.js
+++ frontend/app/components/sparqlet-editor.js
@@ -1,18 +1,4 @@
 import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
 
 export default class SparqletEditor extends Component {
-  @tracked buffer;
-  @tracked name;
-
-  @action
-  updateBuffer(content) {
-    this.buffer = content;
-  }
-
-  @action
-  updateName(name) {
-    this.name = name;
-  }
 }
