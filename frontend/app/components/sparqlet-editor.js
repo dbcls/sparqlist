533d0a838004b4b98bbfc09e29b290f1c7536af2
Keita Urashima <ursm@ursm.jp>
2019-11-09 23:59:08 +0900

    Inline callback of sparqlet-editor component

--- frontend/app/components/sparqlet-editor.js
+++ frontend/app/components/sparqlet-editor.js
@@ -1,20 +1,21 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 import { tracked } from '@glimmer/tracking';
 
 export default class SparqletEditor extends Component {
+  @service router;
+
   @tracked error = null;
 
   @action
   async save() {
-    const {model, onSave} = this.args;
+    const {model} = this.args;
 
     try {
       await model.save();
 
-      if (onSave) {
-        onSave();
-      }
+      this.router.transitionTo('sparqlets.show', model);
     } catch (e) {
       this.error = e;
       // eslint-disable-next-line no-console
