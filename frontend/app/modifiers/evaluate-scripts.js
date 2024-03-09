56072bdee04814b051ba77fdf04330fc68dc120c
Yoji Shidara <dara@shidara.net>
2024-03-09 11:03:34 +0900

    Add `{eager: true}`

--- frontend/app/modifiers/evaluate-scripts.js
+++ frontend/app/modifiers/evaluate-scripts.js
@@ -1,15 +1,18 @@
 import { modifier } from 'ember-modifier';
 
-export default modifier(function evaluateScripts(element) {
-  element.querySelectorAll('script').forEach((orig) => {
-    const el = document.createElement('script');
+export default modifier(
+  function evaluateScripts(element) {
+    element.querySelectorAll('script').forEach((orig) => {
+      const el = document.createElement('script');
 
-    el.textContent = orig.textContent;
+      el.textContent = orig.textContent;
 
-    Array.from(orig.attributes).forEach((attr) => {
-      el.setAttribute(attr.nodeName, attr.textContent);
+      Array.from(orig.attributes).forEach((attr) => {
+        el.setAttribute(attr.nodeName, attr.textContent);
+      });
+
+      orig.replaceWith(el);
     });
-
-    orig.replaceWith(el);
-  });
-});
+  },
+  { eager: true },
+);
