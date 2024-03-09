ed45c34ba6b953e2b635efdad853b74b335cce62
Yoji Shidara <dara@shidara.net>
2024-03-09 10:58:49 +0900

    Update CodeMirrorModifier

--- frontend/app/modifiers/code-mirror.js
+++ frontend/app/modifiers/code-mirror.js
@@ -1,5 +1,3 @@
-import { action } from '@ember/object';
-import { bind } from '@ember/runloop';
 import codemirror from 'codemirror';
 import Modifier from 'ember-modifier';
 
@@ -8,41 +6,26 @@ import 'codemirror/mode/markdown/markdown';
 import 'codemirror/mode/sparql/sparql';
 
 export default class CodeMirrorModifier extends Modifier {
-  didInstall() {
-    this._setup();
-  }
+  didSetup = false;
 
-  didUpdateArguments() {
-    if (this._editor.getValue() !== this.args.named.content) {
-      this._editor.setValue(this.args.named.content);
+  modify(element, _, { content, onUpdate }) {
+    if (!this.didSetup) {
+      const editor = codemirror(element, {
+        lineNumbers: true,
+        matchBrackets: true,
+        mode: 'markdown',
+        readOnly: false,
+        styleActiveLine: true,
+        theme: 'base16-light',
+        value: content || '',
+        viewportMargin: Infinity,
+      });
+
+      editor.on('change', () => {
+        onUpdate(editor.getValue());
+      });
+
+      this.didSetup = true;
     }
-
-    this._editor.setOption('readOnly', this.args.named.readOnly);
-  }
-
-  @action
-  _onChange(editor) {
-    this.args.named.onUpdate(editor.getValue());
-  }
-
-  _setup() {
-    if (!this.element) {
-      throw new Error('CodeMirror modifier has no element');
-    }
-
-    const editor = codemirror(this.element, {
-      lineNumbers: true,
-      matchBrackets: true,
-      mode: 'markdown',
-      readOnly: this.args.named.readOnly,
-      styleActiveLine: true,
-      theme: 'base16-light',
-      value: this.args.named.content || '',
-      viewportMargin: Infinity,
-    });
-
-    editor.on('change', bind(this, this._onChange));
-
-    this._editor = editor;
   }
 }
