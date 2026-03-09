2c00179eb9ad305da2d13dd0d0f170b59353ad8e
Yoji Shidara <dara@shidara.net>
2026-03-09 17:17:48 +0900

    Upgrade dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
+  modulePathIgnorePatterns: ['<rootDir>/frontend/'],
   testMatch: ['<rootDir>/tests/**/*.test.js'],
   transform: {
     '^.+\\.m?js$': 'babel-jest'
-  }
+  },
+  transformIgnorePatterns: ['/node_modules/(?!(parse5|entities)/)']
 };
